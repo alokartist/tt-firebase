44586a13bb09067f3271ff4991d9841c6ec6e705
alokartist <btech60020.22@bitmesra.ac.in>
2025-03-23 22:07:04 +0530

    update

--- startpage/aim/aim-script.js
+++ startpage/aim/aim-script.js
@@ -1,3 +1,18 @@
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", function(event) {
+    if (event.key === "F12" || 
+        (event.ctrlKey && event.shiftKey && (event.key === "I" || event.key === "J")) || 
+        (event.ctrlKey && event.key === "U")) {
+        event.preventDefault();
+    }
+});
+(function() {
+    function devToolsOpen() {
+        debugger;
+    }
+    setInterval(devToolsOpen, 100);
+})();
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
